4bab271d0a43a96c1b0eb249a441c91f771a1370
sivasis das <sivasisd7@gmail.com>
2024-02-01 11:35:45 +0530

    react installed with tailwind css first template

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='uppercase text-orange-600 bg-yellow-400 text-3xl font-bold text-center'>jay jagannath</div>
+  )
+}
+
+export default App
+
