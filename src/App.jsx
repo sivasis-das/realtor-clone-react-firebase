880c55070216a5295856754b3b600f1051cbf4ca
sivasis das <sivasisd7@gmail.com>
2024-02-01 13:43:25 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./Layout";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="home" element={<Home />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="sign-in" element={<SignIn />} />
+      <Route path="sign-up" element={<SignUp />} />
+      <Route path="forgot-password" element={<ForgotPassword />} />
+      <Route path="offers" element={<Offers />} />
+    </Route>
+  )
+);
 function App() {
   return (
-    <div className='uppercase text-orange-600 bg-yellow-400 text-3xl font-bold text-center'>jay jagannath</div>
-  )
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
-
+export default App;
